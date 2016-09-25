d06a3b59e770969f5d996b96641b4a2d2a2149bf
pnakibar <pedronakibar@gmail.com>
2016-09-25 15:09:30 -0300

    like ok

--- js/myindex.js
+++ js/myindex.js
@@ -5,3 +5,20 @@ Array.from(document.getElementsByClassName('like-button')).forEach(e => {
     }
   })
 })
+
+(function() {
+  var updateButton = document.getElementById('updateDetails');
+  var cancelButton = document.getElementById('cancel');
+  var favDialog = document.getElementById('favDialog');
+
+  // Update button opens a modal dialog
+  updateButton.addEventListener('click', function() {
+    favDialog.showModal();
+  });
+
+  // Form cancel button closes the dialog box
+  cancelButton.addEventListener('click', function() {
+    favDialog.close();
+  });
+
+})();
